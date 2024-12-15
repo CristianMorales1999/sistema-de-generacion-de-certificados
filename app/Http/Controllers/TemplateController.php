c7f0d85bb9d548ccd3a76bbbcf15ad0e2a8f874f
CristianMorales1999 <cm9225064@gmail.com>
2024-12-15 18:06:45 -0500

    Vista de tabla de Plantillas en Dasboard

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class TemplateController extends Controller
@@ -11,7 +12,30 @@ class TemplateController extends Controller
      */
     public function index()
     {
-        return view('administrator.templates.index');
+        /*ID		
+        nombre
+        Imagen
+        Estado
+        */
+        $filtered_templates = Image::with([
+            'imageStatus',
+            'imageType'
+        ])
+        ->whereHas('imageType', function ($query) {
+            $query->where('name', 'Plantilla');  // Filtrar por el valor 'plantilla' en el campo 'name' de image_types
+        })
+        ->get();
+
+        // Mapear los datos a la estructura requerida
+        $templates = $filtered_templates->map(function ($templates) {
+            return [
+                'id' => $templates->id,
+                'name'=>$templates->name,
+                'image' => $templates->url,
+                'status' => $templates->imageStatus->name
+            ];
+        });
+        return view('administrator.templates.index',compact('templates'));
     }
 
     /**
