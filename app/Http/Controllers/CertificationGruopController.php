6bdd607f2f4a0454f81018bc0b8b137578e56b89
CristianMorales1999 <cm9225064@gmail.com>
2024-12-15 20:03:41 -0500

    Vista de tabla de CertificationGroups en Dasboard

--- app/Http/Controllers/CertificationGruopController.php
+++ app/Http/Controllers/CertificationGruopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CertificationGroup;
 use Illuminate\Http\Request;
 
 class CertificationGruopController extends Controller
@@ -11,7 +12,57 @@ class CertificationGruopController extends Controller
      */
     public function index()
     {
-        return view('administrator.groups.index');
+        /**
+         * Nombre del grupo de certificación
+         * Tipo de Grupo de certificación
+         * Descripción de grupo de certificación
+         * Fecha de inicio del grupo de certificación
+         * Fecha de fin del grupo de certificación
+         * Usuario Generador de certificados para el grupo de certificación
+         * Usuario Creador del grupo de certificación	
+         * Persona dueña de Firma 1	
+         * Persona dueña de Firma 2	
+         * Persona dueña de Firma 3	
+         * Persona dueña de Firma 4	
+         * Imagen de Tipo Plantilla del Grupo de certificación
+         */
+        $filtered_groups = CertificationGroup::with([
+            'certificationType',
+            'createdBy.person',        // Usuario creador
+            'certifiedBy.person',      // Usuario generador de certificados
+            'signatureImages.person', // Personas dueñas de las firmas
+            'images' => function ($query) {  // Filtramos las imágenes de tipo 'Plantilla'
+                $query->whereHas('imageType', function ($query) {
+                    $query->where('name', 'Plantilla');  // Filtrar por el tipo 'Plantilla'
+                });
+            }
+        ])->get();
+
+        // Mapear los datos a la estructura requerida
+        $groups = $filtered_groups->map(function ($group) {
+            return [
+                'id' => $group->id,
+                'name' => $group->name,
+                'type' => $group->certificationType->name,
+                'description' => $group->description,
+                'start_date' => $group->start_date,
+                'end_date' => $group->end_date,
+                'createdBy' => $group->createdBy->person->first_name." ".$group->createdBy->person->last_name,
+                'certifiedBy' => $group->certifiedBy->person->first_name." ".$group->certifiedBy->person->last_name,
+
+                // Personas dueñas de las firmas (iterar sobre las imágenes de firmas)
+                'signatories' => $group->signatureImages->map(function ($signatureImage) {
+                    return $signatureImage->person->first_name . ' ' . $signatureImage->person->last_name;
+                }),
+
+                // Imágenes de tipo 'Plantilla' (iterar sobre la colección de imágenes)
+                'templates' => $group->images->map(function ($image) {
+                    return $image->url;
+                })//->implode(', ') // Unir los nombres de las imágenes con coma si hay varias
+            ];
+        });
+
+        return view('administrator.groups.index',compact('groups'));
     }
 
     /**
--- resources/views/administrator/groups/index.blade.php
+++ resources/views/administrator/groups/index.blade.php
@@ -1,1 +1,159 @@
-Dashboard-Groups
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+
+@section('content')
+    <div class="main-container" style="background-color: var(--color-primary-50);">
+
+        <div class="dashboard-container">
+            <!-- Menú lateral -->
+            <x-sidebar />
+
+            <!-- Contenido principal -->
+            <section class="dashboard-content">
+                <h2 class="h3">Grupos</h2>
+
+
+                <!-- ============RESPONSIVE TABLE============ -->
+
+                <div class="general-table" style="width: 100%;">
+                    <div class="table-container">
+                        <div class="table-header">
+                            <div class="search-container">
+                                <button class="filter-button">
+                                    <img src="{{ asset('images/icons/icon-filter.svg') }}" alt="Filtro"
+                                        class="filter-icon">
+                                </button>
+                                <div class="search-box">
+                                    <img src="{{ asset('images/icons/icon-search.svg') }}" alt="Buscar"
+                                        class="search-icon">
+                                    <input type="text" id="search" placeholder="Search...">
+                                </div>
+                            </div>
+                            <!--Acciones-->
+                            <div class="action-buttons">
+                                <a href="#" class="button-action btn-small btn-outline">
+                                    <img src="{{ asset('images/icons/icon-plus.svg') }}" alt="Añadir registro">
+                                    Añadir registro
+                                </a>
+                                <a href="#" class="button-action btn-small btn-filled">
+                                    <img src="{{ asset('images/icons/icon-upload.svg') }}" alt="Cargar archivo">
+                                    Cargar archivo
+                                </a>
+                            </div>
+                            <!--Fin Acciones-->
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th><input type="checkbox" id="select-all"></th>
+                                    <th>ID</th>
+                                    <th>Nombre del grupo</th>
+                                    <th>Tipo de certificado</th>
+                                    <th>Descripción</th>
+                                    <th>Fecha de inicio</th>
+                                    <th>Fecha de fin</th>
+                                    <th>Generador de certificados</th>
+                                    <th>Creador del grupo</th>
+                                    <th>Firmante 1</th>
+                                    <th>Firmante 2</th>
+                                    <th>Firmante 3</th>
+                                    <th>Firmante 4</th>
+                                    <th>Plantilla</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Datos dinámicos aquí -->
+                                @foreach ($groups as $group)
+                                    <tr>
+                                        <td><input type="checkbox" class="row-checkbox" data-id="{{ $group['id'] }}">
+                                        </td>
+                                        <td>{{ $group['id'] }}</td>
+                                        <td>{{ $group['name'] }}</td>
+                                        <td>{{ $group['type'] }}</td>
+                                        <td>{{ $group['description'] }}</td>
+                                        <td>{{ $group['start_date'] }}</td>
+
+                                        <td>
+                                            @if ($group['end_date'])
+                                                {{ $group['end_date'] }}
+                                            @else
+                                                Fecha única
+                                            @endif
+                                        </td>
+                                        <td>{{ $group['createdBy'] }}</td>
+                                        <td>
+                                            @if ($group['certifiedBy'])
+                                                {{ $group['certifiedBy'] }}
+                                            @else
+                                                Sin generar
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (count($group['signatories']) > 0)
+                                                {{ $group['signatories'][0] }}
+                                            @else
+                                                Ninguno
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (count($group['signatories']) > 1)
+                                                {{ $group['signatories'][1] }}
+                                            @else
+                                                Ninguno
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (count($group['signatories']) > 2)
+                                                {{ $group['signatories'][2] }}
+                                            @else
+                                                Ninguno
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (count($group['signatories']) > 3)
+                                                {{ $group['signatories'][3] }}
+                                            @else
+                                                Ninguno
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (count($group['templates']) > 0)
+                                                {{ implode(', ', $group['templates']->toArray()) }}
+                                            @else
+                                                Sin Plantilla
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                <!---->
+                            </tbody>
+                        </table>
+                        <div class="pagination">
+                            <span id="rows-info">1-10 de 20</span>
+                            <button class="btn-pagination" id="prev-page" disabled>&laquo; Anterior</button>
+                            <button class="btn-pagination" id="next-page">Siguiente &raquo;</button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </section>
+        </div>
+
+    </div>
+@endsection
+
+@push('styles')
+    <!-- Agregar un archivo CSS específico para esta vista -->
+    @vite('resources/css/common-styles/admin-dashboard-styles.css')
+    @vite('resources/css/common-styles/table-styles.css')
+@endpush
+
+{{-- Este archivo .js lo comento por mientras para que no jala la data ficticia definida en el archivo y asi poder usar la data que capturo aqui de la base de datos mediante los modelos --}}
+{{-- @push('scripts')
+    @vite('resources/js/administrator/groups-table.js')
+@endpush --}}
